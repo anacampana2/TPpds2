6680c33498eeb0b976dac5849f6ad66dc9b31dea
Thales Vasconcelos <thales.gregory@cmtecnologia.com.br>
2019-11-07 16:46:24 -0300

    correcao modularizacao

---
+++ personagem.h
@@ -0,0 +1,30 @@
+#ifndef PERSONAGEM_H
+#define PERSONAGEM_H
+
+#include <stdlib.h>
+#include <string>
+
+
+class Personagem
+{
+public:
+
+    Personagem(int id);
+    void CriaAleatorio();
+    void CriaManual();
+    void Ficha();
+
+protected:
+    
+    int id;
+    float forca;
+    float constituicao;
+    float destreza;
+    float agilidade;
+    float carisma;
+    float vida;
+    float stamina;
+    
+};
+
+#endif
